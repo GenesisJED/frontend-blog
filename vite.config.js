e3edf45549f164538e50939d417981672b6584a6
genesis <escalonagenesis97@gmail.com>
2025-01-23 16:09:43 -0400

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { getEnvVariables } from './src/helpers/getEnvVariables';
+
+const { VITE_API_URL } = getEnvVariables();
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: VITE_API_URL || 'http://localhost:3000',
         secure: false,
       },
     },
