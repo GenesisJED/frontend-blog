ee825e680767dd45d1af1fe143d04bd9f3008c31
genesis <escalonagenesis97@gmail.com>
2025-01-23 16:20:36 -0400

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: import.meta.env.VITE_API_URL || 'http://localhost:3000',
         secure: false,
       },
     },
